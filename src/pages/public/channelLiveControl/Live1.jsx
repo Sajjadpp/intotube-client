7e148761783c70a2bc2a58ca60f2183863c1080b
Sajjadpp <sajjadmuhammed227@gmail.com>
2025-08-14 15:56:08 +0530

    add turn server data

--- src/pages/public/channelLiveControl/Live1.jsx
+++ src/pages/public/channelLiveControl/Live1.jsx
@@ -81,7 +81,14 @@ const LiveStreamerPage = () => {
 
       // Create new peer connection
       const peer = new RTCPeerConnection({
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          {
+            urls: 'relay1.expressturn.com:3480',
+            username: '000000002070527019',
+            credential: 'CZ1M3MvZgZaGiPoVqYpNHeFnHWc='
+          }
+        ]
       });
       
       // Store the connection
